4424e6db1a4b6ac79861952d282a81b402e1075c
totsamiynixon@gmail.com <totsamiynixon@gmail.com>
2018-09-12 04:27:33 +0300

    Ready to use deploy system First Test for TravisCI

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-deployTo = $1
-for f in `curl ftp://$FTP_HOST:$FTP_DEPLOYER_USERNAME@FTP_DEPLOYER_PASSWORD $deployTo/`; do
-
-  # Delete each file individually
-  curl ftp://$FTP_HOST:$FTP_DEPLOYER_USERNAME@FTP_DEPLOYER_PASSWORD -Q "DELE $deployTo/$f"
-done
-find release -type f -exec curl -u $FTP_DEPLOYER_USERNAME:FTP_DEPLOYER_PASSWORD --ftp-create-dirs -T {} ftp://$FTP_HOST/$deployTo/{} \;
